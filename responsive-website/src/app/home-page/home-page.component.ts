6f04b9502624641d4cbf6995387945daeac57191
mennims <mennims@gmail.com>
2022-02-23 15:30:40 +0200

    Implement logo and blog containers

--- responsive-website/src/app/home-page/home-page.component.ts
+++ responsive-website/src/app/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { WebsiteService } from '../website.service';
 
 @Component({
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
-  constructor() { }
+  mobileBreakpoint$ = this.websiteService.mobileBreakpoint$;
+  constructor(private websiteService: WebsiteService) { }
 
   ngOnInit(): void {
   }
